586e81751b9334c04fe3d8e12d55946fbdaafe70
ilyx4ik <ilyxandro7@gmail.com>
2026-06-08 14:26:11 +0200

    prac1_2

--- prac1/prac1.py
+++ prac1/prac1.py
@@ -1,10 +1,2 @@
 name = input("Введіть ваше ім'я: ")
 print(f"Hello, {name}")
-
-print("\n--- Розрахунок суми двох чисел ---")
-num1 = float(input("Введіть перше число: "))
-num2 = float(input("Введіть друге число: "))
-
-Sum = num1 + num2
-
-print(f"Сума чисел {num1} та {num2} дорівнює: {Sum}")
